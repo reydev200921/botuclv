82c3a742f63c80424639f45cb9d47df7170b25a1
l1l4chy <101237622+l1l4chy@users.noreply.github.com>
2022-11-07 20:34:38 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5752454871:AAEcgcmVRwUw6vTdwotvhRzuEsFd3MU9Qcs'
+API_ID = 14169350
+API_HASH = 'c0a4d9a7e42a2bf949e227bfbf59f8cf'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'pcarrazana'
+PASSWORD = 'sims.9812'
+USUARIO_ID = 2830
+ZIP_MB = 398
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = [5257382960]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'quialamaikol@gmail.com'
+PASS_MEGA = '11301410'
